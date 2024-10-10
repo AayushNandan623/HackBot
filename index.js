bf8bfd3a015bb69cc6e6f7106f4605f9ba738bd6
Aayush Nandan <121042190+AayushNandan623@users.noreply.github.com>
2024-10-10 17:50:52 +0530

    updated web scrapper

--- index.js
+++ index.js
@@ -13,40 +13,38 @@ const getCards = async () => {
   });
 
   const data = await page.evaluate(() => {
-    const dataList = document.querySelectorAll(".content");
-
-    if (!dataList || dataList.length === 0) {
-      console.error("No event list found on the page.");
-      return [];
-    }
-
-    return Array.from(dataList).map((event) => {
+    const eventCards = Array.from(
+      document.querySelectorAll(".j-between-start.user_img.opp_content")
+    );
+    return eventCards.map((event) => {
       const title =
         event.querySelector(".double-wrap")?.innerText.trim() || "N/A";
       if (title === "N/A") {
-        return {};
+        return null;
       }
       const institution = event.querySelector("p")?.innerText.trim() || "N/A";
 
-      // Check if prize exists
       const prizeElement = event.querySelector(".prize");
       const prize =
         prizeElement?.innerText.trim().replace(/[^\d,]/g, "") || "No prize";
-
+      if(prize ==="No prize"){
+        return null;
+      }
       const daysLeftElem = event.querySelectorAll(
         ".seperate_box.align-center.ng-star-inserted"
       );
 
       let daysLeft = "Date not available";
       if (daysLeftElem.length === 2) {
-        // Sometimes there are two elements: one for prize, one for days left
+       
         daysLeft = daysLeftElem[1]?.innerText.trim() || "Date not available";
       } else if (daysLeftElem.length === 1) {
-        // If there's only one element, it's likely to be the days left
+     
         daysLeft = daysLeftElem[0]?.innerText.trim() || "Date not available";
       }
-
-      return { title, institution, prize, daysLeft };
+      const imgSrc =
+        event.querySelector("img")?.getAttribute("src") || "No Image";
+      return { title, institution, prize, daysLeft, imgSrc };
     });
   });
 
@@ -56,4 +54,3 @@ const getCards = async () => {
 };
 
 getCards();
-
